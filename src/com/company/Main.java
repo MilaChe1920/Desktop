711fc8cd85c7ead553bf16d6f3800cc2a09f514b
DESKTOP-G008GPR\Admin <1213>
2021-06-03 20:45:48 +0300

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,39 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //    int i;
+        //i = 15;
+        //double d = 16.9;
+        //String s = "Hello, Hillel";
+        //boolean b = false;
+
+
+
+        //System.out.println("Result" +i+d);
+        //System.out.println(s);
+        //System.out.println(b);
+
+        //Пример ввода текста с консоли и его использования
+        Scanner scan = new Scanner(System.in); //объект, который может выполнять сканирование
+
+        System.out.println("Please enter your name");
+
+        String name = scan.nextLine(); // Принимает значение при нажатии на Ентер
+        String surname = scan.nextLine();
+        System.out.println("Hello " + name + surname);
+
+        //Ввод чисел
+        System.out.println("Please enter your age");
+
+        int age = scan.nextInt();
+
+        System.out.println( "In 1 year your age = " + (age+1));
+
+
     }
 }
